1cf2d79d56df94267dfb5d50ad7de6bfa9c7df53
g770728y <g770728y@163.com>
2019-07-05 18:34:23 +0800

    --amend

--- dist/MyComponent/index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-export interface Props {
-    id: number;
-}
-declare const MyComponent: React.FC<Props>;
-export default MyComponent;
--- dist/MyComponent/index.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-var MyComponent = function (_a) {
-    var id = _a.id;
-    return React.createElement("div", null,
-        "\u6536\u5230props: ",
-        id);
-};
-export default MyComponent;
