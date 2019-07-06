a3f0ec92eafb3bf6623d8d79b11400f1b9069cbc
g770728y <g770728y@163.com>
2019-07-06 10:29:40 +0800

    scss & module.scss support

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,29 +5,32 @@ module.exports = ({ config }) => {
     test: /\.(ts|tsx)$/,
     use: [
       {
-        loader: require.resolve('awesome-typescript-loader'),
-      },
-    ],
+        loader: require.resolve('awesome-typescript-loader')
+      }
+    ]
   });
   config.resolve.extensions.push('.ts', '.tsx');
 
+  /****************** WARNING: 在umi项目中, 需要打开以下选项. 但create-react-app项目打开会报错!!!  **********************/
+
   /** 支持sass, see: https://storybook.js.org/docs/configurations/custom-webpack-config/#full-control-mode */
-  config.module.rules.push({
-    test: /\.scss$/,
-    loaders: [
-      require.resolve('style-loader'),
-      /** 支持css module, see: https://github.com/storybookjs/storybook/issues/2320#issuecomment-351804807 */
-      {
-        loader: require.resolve('css-loader'),
-        options: {
-          importLoaders: 1,
-          modules: true,
-          localIdentName: '[name]__[local]___[hash:base64:5]',
-        },
-      },
-      require.resolve('sass-loader'),
-    ],
-    include: path.resolve(__dirname, '../src'),
-  });
+  /** warning: 这个不适合create-react-app项目!!! */
+  // config.module.rules.push({
+  //   test: /\.scss$/,
+  //   loaders: [
+  //     require.resolve('style-loader'),
+  //     /** 支持css module, see: https://github.com/storybookjs/storybook/issues/2320#issuecomment-351804807 */
+  //     {
+  //       loader: require.resolve('css-loader'),
+  //       options: {
+  //         importLoaders: 1,
+  //         modules: true,
+  //         localIdentName: '[name]__[local]___[hash:base64:5]',
+  //       },
+  //     },
+  //     require.resolve('sass-loader'),
+  //   ],
+  //   include: path.resolve(__dirname, '../src'),
+  // });
   return config;
 };
--- src/lib/MyComponent/index.tsx
+++ src/lib/MyComponent/index.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
+import './style.scss';
+import styles from './style.module.scss';
 
 export interface Props {
   id: number;
 }
 const MyComponent: React.FC<Props> = ({ id }) => {
-  return <div>收到props: {id}</div>;
+  return (
+    <div style={{ margin: 'auto', width: 100 }}>
+      <div>收到props: {id}</div>
+      <div>验证在storybook中, 全局的scss与模块化scss均生效</div>
+      <div>
+        以下是普通scss: <div className="common-style" />
+      </div>
+      <div>
+        以下是模块化scss:{' '}
+        <div className={styles['module-style']}>{styles['module-style']}</div>
+      </div>
+    </div>
+  );
 };
 
 export default MyComponent;
